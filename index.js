2f20e9243991c550ff39d0e2a18e9b959be78f44
raphael luiz schettino <raphaelluiz128@hotmail.com>
2021-10-10 12:07:21 -0300

    add function to verify if button search is in page videoaulas

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+async function test(){ 
+
 const webdriver = require('selenium-webdriver');
 let by = webdriver.By;
 
 const driver = new webdriver.Builder().forBrowser('firefox').build();
 
-for (let i = 0; i < 1; i+=1){
+async function login(){
     let email = 'ti-raphael@hotmail.com';
     driver.manage().window().maximize();
 
@@ -13,4 +15,15 @@ for (let i = 0; i < 1; i+=1){
     driver.findElement(by.name('email')).sendKeys(email);
     driver.findElement(by.tagName('button')).click();
 
+    let hasSearch = await driver.wait(webdriver.until.elementLocated(by.name('search')));
+    if( hasSearch ){
+        driver.findElement(by.name('search')).sendKeys('123123');
+    }
 }
+
+await login();
+
+
+}
+
+test();
